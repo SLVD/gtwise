97fe81e853db8539827587578c75cef8ae7d944a
gtwise <gtwise.com>
2019-02-26 18:56:56 +0800

    add js/jquery.js, update else.js

--- else/else.js
+++ else/else.js
@@ -1,2 +1,38 @@
-console.log("Hello World");
-console.log("haha");
+(function ($) {
+    window.IndexedDB = function () {
+        var init = function () {
+            return "xixi";
+        }
+        return {
+            confirm: function (options) {
+                console.log(options.int);
+                console.log($);
+                var s = init();
+                return {
+                    on: function (callback) {
+                        if (callback && callback instanceof Function) {
+                            callback(init());
+                        }
+                    },
+                    self: function (callback) {
+                        if (callback && callback instanceof Function) {
+                            callback("self");
+                        }
+                    }
+
+                }
+            }
+        }
+    }();
+})(jQuery);
+
+
+IndexedDB.confirm({
+    "int": "int",
+    "string": "string",
+    "flag": true,
+    "true": true,
+    "false": false,
+}).self(function (e) {
+    console.log(e);
+});
